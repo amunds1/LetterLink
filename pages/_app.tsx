730b7942479c631fb28fe7890879a511208164c2
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-09-17 17:15:51 +0200

    Switch to light mode globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import { AppProps } from "next/app";
-import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
 
 export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
+  const { Component, pageProps } = props
 
   return (
     <>
@@ -20,11 +20,11 @@ export default function App(props: AppProps) {
         withNormalizeCSS
         theme={{
           /** Put your mantine theme override here */
-          colorScheme: "dark",
+          colorScheme: 'light',
         }}
       >
         <Component {...pageProps} />
       </MantineProvider>
     </>
-  );
+  )
 }
