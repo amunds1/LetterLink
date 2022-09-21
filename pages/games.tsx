30474f8faeeed2ecbec94d272d094a50d2def0e0
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-09-21 16:54:29 +0200

    Display games for a given user

---
+++ pages/games.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { GetServerSideProps } from 'next'
+import fetchGamesByUserID, { Game } from '../firebase/fetch/fetchGamesByUserID'
+import { Card, Image, Text, Badge, Button, Group } from '@mantine/core'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const games = await fetchGamesByUserID('fCRVeYrTPFWyn1UF6QvQ')
+
+  return {
+    props: { games },
+  }
+}
+
+const Games = (games: { games: Game[] }) => {
+  return (
+    <>
+      {games.games.map((game) => {
+        return (
+          <Card key={'abc'} shadow="sm" p="lg" radius="md" withBorder>
+            <Group position="apart" mt="md" mb="xs">
+              <Text weight={500}>Game against {game.oponent}</Text>
+            </Group>
+
+            <Text size="sm" color="dimmed">
+              Boardsize {game.boardSize}
+            </Text>
+
+            <Button variant="light" color="blue" fullWidth mt="md" radius="md">
+              Continue
+            </Button>
+          </Card>
+        )
+      })}
+    </>
+  )
+}
+
+export default Games
