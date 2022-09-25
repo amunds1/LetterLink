872546af4d04bfecb7fc7bbdd92dec3c283c05ba
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-09-25 20:02:50 +0200

    Add basic check board function

--- pages/api/check.ts
+++ pages/api/check.ts
@@ -11,32 +11,43 @@ const wordExists = (word: string[]) => {
   return false
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  // Board prior to move
-  const prevBoard = ['A', 'B', 'C', 'D']
-  // Board after move
-  const nextBoard = ['A', 'B', 'E', 'D']
-
+const checkRow = (prevBoardRow: string[], nextBoardRow: string[]): number => {
   // Find from which index arrays are different
-  for (let i = 0; i < prevBoard.length; i++) {
-    if (prevBoard[i] != nextBoard[i]) {
+  for (let i = 0; i < prevBoardRow.length; i++) {
+    if (prevBoardRow[i] != nextBoardRow[i]) {
       const differentIndex = i
       console.log('Different from index', differentIndex)
 
       // In reverse, check words against Ordbok-API
       // Only checks words up to the differentIndex
-      for (let k = nextBoard.length; k > differentIndex; k--) {
-        const word = nextBoard.slice(0, k)
+      for (let k = nextBoardRow.length; k > differentIndex; k--) {
+        const word = nextBoardRow.slice(0, k)
 
         if (wordExists(word)) {
           console.log('Found valid word:', word)
+
+          return word.length
         }
       }
     }
   }
+
+  return 0
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const rowPoints = [0, 0]
+  const colPoints = [0, 0]
+
+  // Board prior to move
+  const prevBoardRow = ['A', 'B', 'C', 'D']
+  // Board after move
+  const nextBoardRow = ['A', 'B', 'E', 'D']
+
+  console.log('Points for row', checkRow(prevBoardRow, nextBoardRow))
 
   res.status(200).json({ name: 'John Doe' })
 }
