3b9f7908874cbc3634895475bd5e9c4739e937b2
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-09-26 15:57:49 +0200

    Add oponent from select dropdown

--- pages/game/new/index.tsx
+++ pages/game/new/index.tsx
@@ -1,14 +1,19 @@
-import { Button, Select } from '@mantine/core'
+import { Button, Select, SelectItem } from '@mantine/core'
 import {
   addDoc,
   arrayUnion,
   collection,
   doc,
+  getDocs,
+  query,
   updateDoc,
+  where,
 } from 'firebase/firestore'
-import React from 'react'
+import { GetServerSideProps } from 'next'
+import React, { useState } from 'react'
 import { db } from '../../../firebase/clientApp'
 import gamesConverter from '../../../utils/gamesConverter'
+import usersConverter from '../../../utils/userConverter'
 
 const addGameToCollection = async (userDocID: string, oponentDocID: string) => {
   // Generate document refrences to document in users collection, of player and oponent
@@ -42,25 +47,48 @@ const updateUserGamesList = async (gameID: string, userDocID: string) => {
   })
 }
 
-const NewGame = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const options: (string | SelectItem)[] = []
+
+  const q = query(
+    collection(db, 'users'),
+    where('name', '!=', 'Andreas')
+  ).withConverter(usersConverter)
+
+  const querySnapshot = await getDocs(q)
+
+  querySnapshot.forEach((user) => {
+    options.push({ value: user.data().id, label: user.data().name })
+  })
+
+  return {
+    props: { oponentOptions: options },
+  }
+}
+
+const NewGame = ({
+  oponentOptions,
+}: {
+  oponentOptions: (string | SelectItem)[]
+}) => {
+  const [oponent, setOponent] = useState<string | null>(null)
+
   return (
     <>
       <Select
         label="Select oponent"
-        placeholder="Pick one"
-        data={[
-          { value: 'react', label: 'React' },
-          { value: 'ng', label: 'Angular' },
-          { value: 'svelte', label: 'Svelte' },
-          { value: 'vue', label: 'Vue' },
-        ]}
+        placeholder="Pick one oponent"
+        value={oponent}
+        onChange={setOponent}
+        data={oponentOptions}
+        searchable
+        clearable
       />
       <Button
+        disabled={!oponent}
         onClick={() => {
-          addGameToCollection(
-            '5B7aHn9nPMbGj0RvapSacncvdDl1',
-            'sGIx9y9A59NsDw726vKd0HXEDaJ3'
-          )
+          oponent &&
+            addGameToCollection('5B7aHn9nPMbGj0RvapSacncvdDl1', oponent)
         }}
       >
         Add new game
