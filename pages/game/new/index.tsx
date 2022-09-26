91448d374277be9ef3a4e89fd988ec96ea58f3dd
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-09-26 16:14:14 +0200

    Replace with uid

--- pages/game/new/index.tsx
+++ pages/game/new/index.tsx
@@ -1,5 +1,12 @@
 import { Button, Select, SelectItem } from '@mantine/core'
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import {
+  collection,
+  documentId,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore'
 import { GetServerSideProps } from 'next'
 import React, { useState } from 'react'
 import { db } from '../../../firebase/clientApp'
@@ -15,8 +22,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   const q = query(
     collection(db, 'users'),
-    // FIXME Replace hardcoded name. Might use id instead
-    where('name', '!=', 'Andreas')
+    // FIXME Replace hardcoded id
+    where(documentId(), '!=', '5B7aHn9nPMbGj0RvapSacncvdDl1')
   ).withConverter(usersConverter)
 
   const querySnapshot = await getDocs(q)
