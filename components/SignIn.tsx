0c3e016b1f20bb8bb3a81356c419e1328995eb56
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-08 12:25:35 +0200

    Center signin form

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,18 +1,24 @@
-import { Button, Space } from '@mantine/core'
+import { Button, Center, Container, createStyles, Space } from '@mantine/core'
 import React from 'react'
 import firebase from '../firebase/clientApp'
 import { useSignInWithGithub } from 'react-firebase-hooks/auth'
 import { getAuth } from 'firebase/auth'
 
+const useStyles = createStyles(() => ({
+  container: { height: '100%', width: '10%', backgroundColor: 'red' },
+}))
+
 const SignIn = () => {
+  const { classes } = useStyles()
+
   const [signInWithGithub] = useSignInWithGithub(getAuth(firebase))
 
   return (
-    <>
+    <Container className={classes.container}>
       <a>Click here to sign in</a>
       <Space h="md" />
       <Button onClick={() => signInWithGithub()}>Sign in using GitHub</Button>
-    </>
+    </Container>
   )
 }
 
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,12 +1,19 @@
+import { Center, Container, createStyles } from '@mantine/core'
 import React from 'react'
 import { AuthenticationForm } from '../components/SignIn/AuthenticationForm'
 
-const signin = () => {
+const useStyles = createStyles(() => ({
+  container: { height: '100%' },
+}))
+
+const SignIn = () => {
+  const { classes } = useStyles()
+
   return (
-    <>
+    <Center className={classes.container}>
       <AuthenticationForm />
-    </>
+    </Center>
   )
 }
 
-export default signin
+export default SignIn
