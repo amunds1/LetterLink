2c05518e58004d28e6cd27ccaaa9431a22b0d3d8
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-08 12:48:56 +0200

    Improve design of signout page

--- pages/signout.tsx
+++ pages/signout.tsx
@@ -1,7 +1,8 @@
-import { Button, Center, createStyles, Space, Text } from '@mantine/core'
+import { Center, createStyles, Text } from '@mantine/core'
 import { getAuth, signOut } from 'firebase/auth'
 import Link from 'next/link'
 import { useEffect } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import firebase from '../firebase/clientApp'
 
 const useStyles = createStyles(() => ({
@@ -15,9 +16,21 @@ const SignOut = () => {
     signOut(getAuth(firebase))
   }, [])
 
+  const [user, loading, error] = useAuthState(getAuth(firebase))
+
   return (
     <Center className={classes.center}>
-      <Text size={'xl'}>Successfully signed out</Text>
+      {!user && !loading && (
+        <Text size={'xl'}>
+          You were never logged in. Did you mean to{' '}
+          <Link href={'/signin'}>
+            <a>Sign In</a>
+          </Link>
+          ?
+        </Text>
+      )}
+
+      {user && !loading && <Text size={'xl'}>Successfully signed out</Text>}
     </Center>
   )
 }
