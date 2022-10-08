c3993755cb7d3038a9e3ec12f118f2d2fb7ad9c8
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-08 12:30:25 +0200

    Update signout page

--- pages/signout.tsx
+++ pages/signout.tsx
@@ -1,13 +1,25 @@
-import { signOut, getAuth } from 'firebase/auth'
-import React, { useEffect } from 'react'
+import { Button, Center, createStyles, Space, Text } from '@mantine/core'
+import { getAuth, signOut } from 'firebase/auth'
+import Link from 'next/link'
+import { useEffect } from 'react'
 import firebase from '../firebase/clientApp'
 
+const useStyles = createStyles(() => ({
+  center: { height: '100%' },
+}))
+
 const SignOut = () => {
+  const { classes } = useStyles()
+
   useEffect(() => {
     signOut(getAuth(firebase))
   }, [])
 
-  return <div>Successfully signed out</div>
+  return (
+    <Center className={classes.center}>
+      <Text size={'xl'}>Successfully signed out</Text>
+    </Center>
+  )
 }
 
 export default SignOut
