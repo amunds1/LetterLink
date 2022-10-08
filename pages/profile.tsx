f661bdc8a20efff6ad2cc52dd1fff0aec34b72c6
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-08 16:24:06 +0200

    Change stats design

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,6 +2,7 @@ import {
   Avatar,
   Center,
   createStyles,
+  Grid,
   Group,
   RingProgress,
   Stack,
@@ -15,52 +16,60 @@ const useStyles = createStyles(() => ({
 const Profile = () => {
   const { classes } = useStyles()
 
-  const ringSize = 80
+  const ringSize = 100
   const ringThickness = 5
 
   return (
     <Center className={classes.center}>
-      <Stack justify="flex-start">
-        <Stack align="center">
-          <RingProgress
-            size={ringSize}
-            thickness={ringThickness}
-            sections={[{ value: 100, color: 'teal' }]}
-            label={
-              <Center>
-                <Text color="green" weight={700} align="center" size="md">
-                  7
+      <Grid>
+        <Grid.Col span={6}>
+          <Stack align="center">
+            <RingProgress
+              size={ringSize}
+              thickness={ringThickness}
+              sections={[{ value: 100, color: 'teal' }]}
+              label={
+                <Center>
+                  <Text color="green" weight={700} align="center" size="md">
+                    7
+                  </Text>
+                </Center>
+              }
+            />
+            <Text size={'md'}>Games played</Text>
+          </Stack>
+        </Grid.Col>
+
+        <Grid.Col span={6}>
+          <Stack align="center">
+            <RingProgress
+              size={ringSize}
+              thickness={ringThickness}
+              sections={[{ value: 42, color: 'blue' }]}
+              label={
+                <Text color="blue" weight={700} align="center" size="md">
+                  42%
                 </Text>
-              </Center>
-            }
-          />
-          <Text size={'md'}>Games played</Text>
-        </Stack>
+              }
+            />
+            <Text size={'md'}>Win rate</Text>
+          </Stack>
+        </Grid.Col>
 
-        <Stack align="center">
-          <RingProgress
-            size={ringSize}
-            thickness={ringThickness}
-            sections={[{ value: 42, color: 'blue' }]}
-            label={
-              <Text color="blue" weight={700} align="center" size="md">
-                42%
-              </Text>
-            }
-          />
-          <Text size={'md'}>Win rate</Text>
-        </Stack>
+        <Grid.Col span={6}>
+          <Stack align="center">
+            <Avatar size={ringSize} />
+            <Text size={'md'}>Most wins against</Text>
+          </Stack>
+        </Grid.Col>
 
-        <Stack align="center">
-          <Avatar size={ringSize} />
-          <Text size={'md'}>Most wins against</Text>
-        </Stack>
-
-        <Stack align="center">
-          <Avatar size={ringSize} />
-          <Text size={'md'}>Most defeats against</Text>
-        </Stack>
-      </Stack>
+        <Grid.Col span={6}>
+          <Stack align="center">
+            <Avatar size={ringSize} />
+            <Text size={'md'}>Most defeats against</Text>
+          </Stack>
+        </Grid.Col>
+      </Grid>
     </Center>
   )
 }
