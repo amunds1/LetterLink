239a41f24b814a92810f7aee731b94b1430293cc
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-13 13:00:56 +0200

    Conditional render reject/accept game proposal

--- components/games/GameProposal.tsx
+++ components/games/GameProposal.tsx
@@ -1,15 +1,21 @@
 import { Button, Card, Group, Badge, Center, Avatar } from '@mantine/core'
+import { doc } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
+import { db } from '../../firebase/clientApp'
 import Game from '../../types/Game'
 import Oponent from '../Oponent'
 import acceptProposedGame from './firebase/acceptProposedGame'
 import rejectProposedGame from './firebase/rejectProposedGame'
+import withdrawGameProposal from './firebase/withdrawGameProposal'
 
 interface IGameProposal {
   game: Game
+  userUID: string
 }
 
-const GameProposal = ({ game }: IGameProposal) => {
+const GameProposal = ({ game, userUID }: IGameProposal) => {
+  const userDocRef = doc(db, `users/${userUID}`)
+
   const [gameProposedTimeDelta, setGameProposedTimeDelta] = useState<{
     value: number
     label: string
@@ -41,45 +47,75 @@ const GameProposal = ({ game }: IGameProposal) => {
           AB
         </Avatar>
       </Center>
-      <Button
-        onClick={async () => {
-          await acceptProposedGame({
-            gameID: game.id!,
-            userRef: game.playerOne,
-          })
-          await acceptProposedGame({
-            gameID: game.id!,
-            userRef: game.playerTwo,
-          })
-        }}
-        variant="light"
-        color="green"
-        fullWidth
-        mt="md"
-        radius="md"
-      >
-        Accept
-      </Button>
-      <Center>
+
+      {userDocRef.id != game.proposedBy.id && (
+        <>
+          {/* ACCEPT PROPOSAL BUTTON */}
+          <Button
+            onClick={async () => {
+              await acceptProposedGame({
+                gameID: game.id!,
+                userRef: game.playerOne,
+              })
+              await acceptProposedGame({
+                gameID: game.id!,
+                userRef: game.playerTwo,
+              })
+            }}
+            variant="light"
+            color="green"
+            fullWidth
+            mt="md"
+            radius="md"
+          >
+            Accept
+          </Button>
+
+          <Center>
+            {/* REJECT PROPOSAL BUTTON */}
+            <Button
+              variant="subtle"
+              color="red"
+              mt="md"
+              radius="md"
+              onClick={async () => {
+                await rejectProposedGame({
+                  gameID: game.id!,
+                  userRef: game.playerOne,
+                })
+                await rejectProposedGame({
+                  gameID: game.id!,
+                  userRef: game.playerTwo,
+                })
+              }}
+            >
+              Reject
+            </Button>
+          </Center>
+        </>
+      )}
+
+      {userDocRef.id == game.proposedBy.id && (
         <Button
-          variant="subtle"
-          color="red"
-          mt="md"
-          radius="md"
           onClick={async () => {
-            await rejectProposedGame({
+            await withdrawGameProposal({
               gameID: game.id!,
               userRef: game.playerOne,
             })
-            await rejectProposedGame({
+            await withdrawGameProposal({
               gameID: game.id!,
               userRef: game.playerTwo,
             })
           }}
+          variant="light"
+          color="red"
+          fullWidth
+          mt="md"
+          radius="md"
         >
-          Reject
+          Withdraw proposal
         </Button>
-      </Center>
+      )}
     </Card>
   )
 }
