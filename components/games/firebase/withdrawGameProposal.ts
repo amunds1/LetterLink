9a2be5137cf20cf4735df8e283acda778761954d
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-13 13:00:32 +0200

    Add withdrawGameProposal function

---
+++ components/games/firebase/withdrawGameProposal.ts
@@ -0,0 +1,16 @@
+import { DocumentReference, DocumentData } from 'firebase/firestore'
+import rejectProposedGame from './rejectProposedGame'
+
+interface IWidthdrawGameProposal {
+  userRef: DocumentReference<DocumentData>
+  gameID: string
+}
+
+const withdrawGameProposal = ({ userRef, gameID }: IWidthdrawGameProposal) => {
+  rejectProposedGame({
+    gameID,
+    userRef,
+  })
+}
+
+export default withdrawGameProposal
