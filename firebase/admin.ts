a18cd3f048fc679ca1343d3a016ee680187ec9b3
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-16 15:50:41 +0200

    Initialize admin app

---
+++ firebase/admin.ts
@@ -0,0 +1,20 @@
+// firebaseAdmin.ts
+
+import * as firebaseAdmin from 'firebase-admin'
+
+// get this JSON from the Firebase board
+// you can also store the values in environment variables
+import serviceAccount from '../serviceAccountKey.json'
+
+if (!firebaseAdmin.apps.length) {
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert({
+      privateKey: serviceAccount.private_key,
+      clientEmail: serviceAccount.client_email,
+      projectId: serviceAccount.project_id,
+    }),
+    databaseURL: 'https://masterthesis-5x5game.firebaseio.com',
+  })
+}
+
+export { firebaseAdmin }
