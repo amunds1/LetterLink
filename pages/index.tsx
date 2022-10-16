bf81003775410a2dadc9e7a8110f4a26e4f2f41c
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-16 17:11:57 +0200

    Cleanup unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
 import { Center, createStyles, Text } from '@mantine/core'
-import { getAuth } from 'firebase/auth'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import firebase from '../firebase/clientApp'
 
 const useStyles = createStyles(() => ({
   center: { height: '100%' },
@@ -11,15 +6,6 @@ const useStyles = createStyles(() => ({
 
 export default function Home() {
   const { classes } = useStyles()
-
-  const [user, loading, error] = useAuthState(getAuth(firebase))
-  const router = useRouter()
-
-  console.log('Loading:', loading, '|', 'Current user:', user?.uid)
-
-  useEffect(() => {
-    console.log(user, loading)
-  }, [user, loading, router])
 
   return (
     <Center className={classes.center}>
