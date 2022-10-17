f40818528d7f9d9c5c43c19ae7699d952d19342c
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-17 10:55:36 +0200

    Update AuthProvider according to tutorial

--- firebase/AuthProvider.tsx
+++ firebase/AuthProvider.tsx
@@ -1,37 +1,45 @@
-import { User } from '@firebase/auth'
-import { getAuth } from 'firebase/auth'
+import React, { useState, useEffect, useContext, createContext } from 'react'
 import nookies from 'nookies'
-import { createContext, useContext, useEffect, useState } from 'react'
+
+import { getAuth, User } from 'firebase/auth'
 import firebase from './clientApp'
 
-const AuthContext = createContext<{ user: User | null }>({ user: null })
+const AuthContext = createContext<{ user: User | null }>({
+  user: null,
+})
 
-const AuthProvider = ({ children }: any) => {
+export function AuthProvider({ children }: any) {
   const [user, setUser] = useState<User | null>(null)
 
-  // listen for token changes
-  // call setUser and write new token as a cookie
   useEffect(() => {
+    if (typeof window !== 'undefined') {
+      ;(window as any).nookies = nookies
+    }
     return getAuth(firebase).onIdTokenChanged(async (user) => {
+      console.log(`token changed!`)
       if (!user) {
+        console.log(`no token found...`)
         setUser(null)
-        nookies.set(undefined, 'token', '', { path: '/' })
-      } else {
-        const token = await user.getIdToken()
-        setUser(user)
-        nookies.set(undefined, 'token', token, { path: '/' })
+        nookies.destroy(null, 'token')
+        nookies.set(null, 'token', '', { path: '/' })
+        return
       }
+
+      console.log(`updating token...`)
+      const token = await user.getIdToken()
+      setUser(user)
+      nookies.destroy(null, 'token')
+      nookies.set(null, 'token', token, { path: '/' })
     })
   }, [])
 
   // force refresh the token every 10 minutes
   useEffect(() => {
     const handle = setInterval(async () => {
+      console.log(`refreshing token...`)
       const user = getAuth(firebase).currentUser
       if (user) await user.getIdToken(true)
     }, 10 * 60 * 1000)
-
-    // clean up setInterval
     return () => clearInterval(handle)
   }, [])
 
@@ -43,5 +51,3 @@ const AuthProvider = ({ children }: any) => {
 export const useAuth = () => {
   return useContext(AuthContext)
 }
-
-export default AuthProvider
