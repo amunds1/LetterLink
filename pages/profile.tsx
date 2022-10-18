990b9dbfb6f160de62307c1edf0658ed825c05e3
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-10-18 19:22:33 +0200

    Extract helper function

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,7 @@
 import { Center, createStyles, Text } from '@mantine/core'
 import { doc, getDoc } from 'firebase/firestore'
 import { GetServerSidePropsContext } from 'next'
+import { fetchUserData } from '../components/profile/firebase/fetchUserData'
 import Statistics from '../components/profile/Statistics'
 import { db } from '../firebase/clientApp'
 import usersConverter from '../firebase/converters/userConverter'
@@ -13,9 +14,7 @@ const useStyles = createStyles(() => ({
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   try {
     const uid = await fetchUID(ctx)
-
-    const userDocRef = doc(db, `users/${uid}`).withConverter(usersConverter)
-    const userData = (await getDoc(userDocRef)).data()
+    const userData = await fetchUserData(uid)
 
     // Can not directly pass userData because proposedGames and games are not serializable
 
@@ -39,12 +38,14 @@ interface IProfile {
 }
 
 const Profile = (props: IProfile) => {
+  const { userData } = props
+
   const { classes } = useStyles()
 
   return (
     <>
       <Center>
-        <Text size="lg">{props.userData.name}</Text>
+        <Text size="lg">{userData.name}</Text>
       </Center>
       <Statistics />
     </>
