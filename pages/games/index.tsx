0088d9e32f9d44293a916b0a6d450b40e68beb4b
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-12-02 14:23:52 +0100

    Set costum cache header for /games

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -17,6 +17,13 @@ const useStyles = createStyles(() => ({
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   try {
+    // Set cache header
+    // https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props#caching-with-server-side-rendering-ssr
+    ctx.res.setHeader(
+      'Cache-Control',
+      'public, s-maxage=10, stale-while-revalidate=59'
+    )
+
     let activeGames: Game[] | null = null
     let proposedGames: Game[] | null = null
 
