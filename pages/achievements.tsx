1f49eea8d8d8997d68f7e27c4b80533ff11e8496
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-12-02 14:49:46 +0100

    Update /achievements with mock data

--- pages/achievements.tsx
+++ pages/achievements.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { Grid, Stack, Text } from '@mantine/core'
 
-const achievements = () => {
-  return <div>achievements</div>
+import { IconMedal } from '@tabler/icons'
+
+const Acheviement = ({ unlocked }: { unlocked: boolean }) => {
+  return (
+    <Stack align={'center'}>
+      <IconMedal color={unlocked ? 'gold' : 'gray'} size={80} />
+      <Text align="center">Play 10 games</Text>
+    </Stack>
+  )
 }
 
-export default achievements
+const Achievements = () => {
+  return (
+    <>
+      <Grid>
+        <Grid.Col span={4}>
+          <Acheviement unlocked={true} />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Acheviement unlocked={false} />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Acheviement unlocked={false} />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Acheviement unlocked={false} />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Acheviement unlocked={true} />
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Acheviement unlocked={false} />
+        </Grid.Col>
+      </Grid>
+    </>
+  )
+}
+
+export default Achievements
