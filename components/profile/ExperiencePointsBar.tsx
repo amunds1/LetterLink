f03dd2f7e0ab1e0a0f145f755a83aa0460f0ea34
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-12-05 19:31:26 +0100

    Retrieve and show experience points stored on user doc

--- components/profile/ExperiencePointsBar.tsx
+++ components/profile/ExperiencePointsBar.tsx
@@ -1,12 +1,21 @@
 import { Progress, Stack, Text } from '@mantine/core'
 
-const ExperiencePointsBar = () => {
+const ExperiencePointsBar = ({
+  experiencePoints,
+}: {
+  experiencePoints: number
+}) => {
   return (
     <Stack style={{ width: '60%' }}>
       <Text align="center">Level 2</Text>
-      <Progress color="green" size="xl" value={70} label={'70 XP'} />
+      <Progress
+        color="green"
+        size="xl"
+        value={experiencePoints}
+        label={`${experiencePoints} XP`}
+      />
       <Text align="center" size={'sm'}>
-        Earn just 30 more XP points to reach level 3!
+        Earn just {100 - experiencePoints} more XP points to reach level 3!
       </Text>
     </Stack>
   )
