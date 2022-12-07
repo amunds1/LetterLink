ca6a48864b4bd25171efd535461384eff2f061c7
Andreas Amundsen <andreas.amundsen123@gmail.com>
2022-12-07 10:29:35 +0100

    Add transition to notification

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,6 +1,7 @@
-import { Center, Stack } from '@mantine/core'
+import { Transition } from '@mantine/core'
 import { GetServerSidePropsContext } from 'next'
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { SuccessNotification } from '../components/NotificationBanner'
 import ChangeUsername from '../components/settings/ChangeUsername'
 import fetchUID from '../firebase/fetchUID'
 
@@ -24,8 +25,33 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
 }
 
 const Settings = ({ uid }: { uid: string }) => {
+  const [alert, setAlert] = useState(true)
+
+  // Remove alert after 3 seconds
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+  }, [])
+
   return (
     <>
+      <Transition
+        mounted={alert}
+        transition="fade"
+        duration={800}
+        timingFunction="ease"
+      >
+        {(styles) => (
+          <div style={styles}>
+            <SuccessNotification
+              title="Success"
+              contentMessage="Successfully updated profile information"
+            />
+          </div>
+        )}
+      </Transition>
+
       <ChangeUsername uid={uid} />
     </>
   )
