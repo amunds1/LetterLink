bfcd38ac5c1647f31681a2af8244af9e814a2fc2
Andreas Amundsen <andreas.amundsen123@gmail.com>
2023-01-17 14:13:02 +0100

    Add concent page

---
+++ pages/concent.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { Button, Checkbox, Stack } from '@mantine/core'
+
+const Concent = () => {
+  const [checked, setChecked] = useState(false)
+
+  return (
+    <>
+      <p>Letterlink is a ...</p>
+      <Stack justify={'center'} align={'center'}>
+        <Checkbox
+          checked={checked}
+          onChange={(event) => setChecked(event.currentTarget.checked)}
+          label={'I agree to participate'}
+        />
+        <Button color={'green'} disabled={!checked}>
+          Continue
+        </Button>
+      </Stack>
+    </>
+  )
+}
+
+export default Concent
