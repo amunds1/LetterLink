4c6b44e41d7617178b454e76d94e2834adcff7f6
Andreas Amundsen <andreas.amundsen123@gmail.com>
2023-01-19 17:14:26 +0100

    Refactor and change styling of ActiveGames

--- components/games/ActiveGames.tsx
+++ components/games/ActiveGames.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Card, Group, Button, Text } from '@mantine/core'
+import { Card, Group, Button, Text, Avatar, Stack } from '@mantine/core'
 import Game from '../../types/Game'
 import Oponent from './Oponent'
 
@@ -8,39 +8,73 @@ interface IActiveGames {
   games: Game[] | null
 }
 
-const ActiveGames = ({ games, userUID }: IActiveGames) => {
+interface IActiveGame {
+  game: Game
+  uid: string
+}
+const ActiveGame = ({ game, uid }: IActiveGame) => {
   return (
     <>
+      <Group
+        position="apart"
+        mb="sm"
+        key={game.id}
+        style={{
+          border: '2px solid green',
+          backgroundColor: '#ebfbee',
+          borderRadius: '5px',
+          padding: '0.5rem',
+        }}
+      >
+        <Stack>
+          <Group spacing="xl">
+            <Oponent game={game} userUID={uid} />
+            <Avatar color="orange" radius="xl">
+              KL
+            </Avatar>
+          </Group>
+          <Group spacing="xl">
+            <Text color="#495057">Rounds left: 8</Text>
+            <Text color="#495057">
+              Boardsize: {game.boardSize}x{game.boardSize}
+            </Text>
+          </Group>
+        </Stack>
+        <Link
+          href={`/game/${game.id}`}
+          style={{ color: 'inherit', textDecoration: 'none' }}
+        >
+          <Button color="green" variant="filled">
+            Open game
+          </Button>
+        </Link>
+      </Group>
+    </>
+  )
+}
+
+const ActiveGames = ({ games, userUID }: IActiveGames) => {
+  return (
+    <Card shadow="sm" p="sm" radius="md" withBorder>
+      <Text align="left" size="lg" pb="xl">
+        Active games
+      </Text>
       {games &&
         games.map((game) => {
           return (
-            <Card key={game.id} shadow="sm" p="lg" radius="md" withBorder>
-              <Group position="apart" mt="md" mb="xs">
-                <Oponent game={game} userUID={userUID} />
-              </Group>
-
-              <Text size="sm" color="dimmed">
-                Boardsize {game.boardSize}
+            <>
+              <Text size="lg" pb="sm">
+                Your turn
               </Text>
+              <ActiveGame game={game} uid={userUID} />
 
-              <Link
-                href={`/game/${game.id}`}
-                style={{ color: 'inherit', textDecoration: 'none' }}
-              >
-                <Button
-                  variant="light"
-                  color="blue"
-                  fullWidth
-                  mt="md"
-                  radius="md"
-                >
-                  Continue
-                </Button>
-              </Link>
-            </Card>
+              <Text size="lg" pb="sm">
+                Their turn
+              </Text>
+            </>
           )
         })}
-    </>
+    </Card>
   )
 }
 
