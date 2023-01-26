acfad71ee2f3bbc3366b6f0a8990846e2279b6c0
Andreas Amundsen <andreas.amundsen123@gmail.com>
2023-01-26 14:40:27 +0100

    Improve achievement structure

--- pages/achievements.tsx
+++ pages/achievements.tsx
@@ -1,12 +1,51 @@
-import { Grid, Stack, Text } from '@mantine/core'
+import { Grid, Progress, Stack, Text } from '@mantine/core'
 
 import { IconMedal } from '@tabler/icons'
 
-const Acheviement = ({ unlocked }: { unlocked: boolean }) => {
+interface IAchievement {
+  title: string
+  range: number
+  completionStatus: number
+  unlocked: boolean
+}
+
+const mockData: { [key: string]: IAchievement } = {
+  'play-10-games': {
+    title: 'Play 10 games',
+    range: 10,
+    completionStatus: 5,
+    unlocked: false,
+  },
+  'win-3-games': {
+    title: 'Win 3 games',
+    range: 3,
+    completionStatus: 2,
+    unlocked: false,
+  },
+  'play-three-opponents': {
+    title: 'Play three different opponents',
+    range: 3,
+    completionStatus: 3,
+    unlocked: true,
+  },
+}
+
+const updateAchievementStatus = () => {
+  // Play 10 games
+
+  // Win 3 games
+  if (mockData['win-3-games'].unlocked == false) {
+  }
+
+  // Play three different opponents
+}
+
+const Acheviement = ({ data }: { data: IAchievement }) => {
   return (
-    <Stack align="center">
-      <IconMedal color={unlocked ? 'gold' : 'gray'} size={80} />
-      <Text align="center">Play 10 games</Text>
+    <Stack>
+      <IconMedal color={data.unlocked ? 'gold' : 'gray'} size={80} />
+      <Text align="center">{data.title}</Text>
+      <Progress size="xl" value={100 * (data.completionStatus / data.range)} />
     </Stack>
   )
 }
@@ -15,24 +54,11 @@ const Achievements = () => {
   return (
     <>
       <Grid>
-        <Grid.Col span={4}>
-          <Acheviement unlocked={true} />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Acheviement unlocked={false} />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Acheviement unlocked={false} />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Acheviement unlocked={false} />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Acheviement unlocked={true} />
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Acheviement unlocked={false} />
-        </Grid.Col>
+        {/* {mockData.values().map((achievement) => (
+          <Grid.Col key={achievement.title} span={4}>
+            <Acheviement data={achievement} />
+          </Grid.Col>
+        ))} */}
       </Grid>
     </>
   )
