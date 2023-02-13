3003b2b4c1b1eb0dad3403c0759e73defebfb212
krlarss <krlarss@stud.ntnu.no>
2023-02-13 12:36:34 +0100

    Added levelnames

--- components/profile/ExperiencePointsBar.tsx
+++ components/profile/ExperiencePointsBar.tsx
@@ -1,13 +1,23 @@
 import { Progress, Stack, Text } from '@mantine/core'
+import Levels, { ILevels } from '../../constants/Levels'
 
 const ExperiencePointsBar = ({
   experiencePoints,
 }: {
   experiencePoints: number
 }) => {
+  // Calculate level
+  const level = Math.floor(experiencePoints / 50)
+
+  // Find levelName for that level
+  const levelList: ILevels = Levels
+  const levelName: string = levelList[level]
+
   return (
     <Stack>
-      <Text align="center">Level 2</Text>
+      <Text align="center">
+        Level {level} - {levelName}
+      </Text>
       <Progress
         color="green"
         size="xl"
@@ -15,7 +25,9 @@ const ExperiencePointsBar = ({
         label={`${experiencePoints} XP`}
       />
       <Text align="center" size="sm">
-        Earn just {100 - experiencePoints} more XP points to reach level 3!
+        Earn just {(level + 1) * 50 - experiencePoints} more XP points to reach
+        Level {''}
+        {level + 1}!
       </Text>
     </Stack>
   )
---
+++ constants/Levels.ts
@@ -0,0 +1,34 @@
+export interface ILevels {
+  [key: number]: string
+}
+
+const Levels = {
+  1: 'Beginner',
+  2: 'Word Warrior',
+  3: 'Letter Legend',
+  4: 'Alphabet Ace',
+  5: 'Scrabble star',
+  6: 'Crossword Crusader',
+  7: 'Word Wizard',
+  8: 'Word Champion',
+  9: 'Puzzle Pro',
+  10: 'Letter Genius',
+  11: 'Word Master',
+  12: 'Alphabet Hero',
+  13: 'Word Champion',
+  14: 'Crossword King',
+  15: 'Scrabble Savant',
+  16: 'Letter Boss',
+  17: 'Word Winner',
+  18: 'Puzzle Pal',
+  19: 'Alphabet Adept',
+  20: 'Crossword Connoisseur',
+  21: 'Word Ace',
+  22: 'Letter Champion',
+  23: 'Word Whisperer',
+  24: 'Puzzle Royal',
+  25: 'Scrabble Supreme',
+  26: 'Crossword Champion',
+}
+
+export default Levels
